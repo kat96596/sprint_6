e18e5fd48ab510ddb4b60a9549e4a4eb4ac4ab60
kat96596 <82775290+kat96596@users.noreply.github.com>
2026-03-19 21:48:16 +0500

    Update test_order_flow.py

--- tests/test_order_flow.py
+++ tests/test_order_flow.py
@@ -5,9 +5,8 @@ from pages.order_page import OrderPage
 from pages.dzen_page import DzenPage
 from data import Urls, OrderData
 
-
 @allure.feature('Заказ самоката')
-class TestOrderFlow:
+class TestOrderCreation:
 
     @allure.title('Позитивный сценарий заказа самоката')
     @allure.description('Тест проверяет полный флоу заказа самоката с разными наборами данных и точками входа')
@@ -17,38 +16,50 @@ class TestOrderFlow:
     ])
     def test_create_order_positive(self, driver, order_data, entry_point):
         main_page = MainPage(driver)
-        main_page.driver.get(Urls.BASE_URL)
+        main_page.open(Urls.BASE_URL)
         main_page.accept_cookies()
 
-        order_page = OrderPage(driver)
-
         if entry_point == 'header':
-            result = order_page.create_order(order_data, main_page.click_order_button_header)
+            main_page.click_order_button_header()
         else:
-            result = order_page.create_order(order_data, main_page.click_order_button_middle)
+            main_page.click_order_button_middle()
 
-        assert result, "Заказ не был успешно создан"
+        order_page = OrderPage(driver)
+        order_page.fill_customer_form(order_data)
+        order_page.click_next_button()
+        order_page.fill_rental_form(order_data)
+        order_page.click_order_button()
+        order_page.confirm_order()
+
+        assert order_page.is_success_modal_displayed(), "Заказ не был успешно создан"
+
+
+@allure.feature('Навигация')
+class TestNavigation:
 
     @allure.title('Проверка перехода на главную страницу по клику на логотип "Самокат"')
     @allure.description('Тест проверяет, что клик на логотип "Самокат" возвращает на главную страницу')
     def test_scooter_logo_redirect(self, driver):
         main_page = MainPage(driver)
-        main_page.driver.get(Urls.BASE_URL)
+        main_page.open(Urls.BASE_URL)
 
         main_page.click_order_button_header()
-
         order_page = OrderPage(driver)
-        order_page.wait_for_element_visible(order_page.NAME_INPUT)
+        order_page.wait_for_element_visible(order_page.locators.NAME_INPUT)
 
         main_page.click_scooter_logo()
 
         assert main_page.get_current_url() == Urls.BASE_URL, "Не произошел переход на главную страницу Самоката"
 
+
+@allure.feature('Внешние ссылки')
+class TestExternalLinks:
+
     @allure.title('Проверка перехода на Дзен по клику на логотип "Яндекс"')
     @allure.description('Тест проверяет, что клик на логотип "Яндекс" открывает страницу Дзена в новом окне')
     def test_yandex_logo_redirect(self, driver):
         main_page = MainPage(driver)
-        main_page.driver.get(Urls.BASE_URL)
+        main_page.open(Urls.BASE_URL)
 
         main_page.click_yandex_logo()
 
@@ -56,6 +67,5 @@ class TestOrderFlow:
 
         dzen_page = DzenPage(driver)
         dzen_page.wait_for_page_load()
-
         current_url = main_page.get_current_url()
         assert Urls.DZEN_URL in current_url, f"Ожидался переход на Дзен, получен URL: {current_url}"
