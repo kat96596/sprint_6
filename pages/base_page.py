1c925eb650e2bf47e8100e04172bad19b30cd036
kat96596 <82775290+kat96596@users.noreply.github.com>
2026-03-19 21:45:26 +0500

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,9 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
+    def open(self, url):
+        self.driver.get(url)
+
     def find_element(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
                                                       message=f"Элемент {locator} не найден")
@@ -27,12 +30,35 @@ class BasePage:
     def wait_for_element_visible(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(EC.visibility_of_element_located(locator))
 
+    def wait_for_element_invisible(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(EC.invisibility_of_element_located(locator))
+
     def switch_to_new_window(self):
         WebDriverWait(self.driver, 10).until(EC.number_of_windows_to_be(2))
+        
+        windows = self.driver.window_handles
         original_window = self.driver.current_window_handle
-        new_window = [window for window in self.driver.window_handles if window != original_window][0]
-        self.driver.switch_to.window(new_window)
+        
+        for window in windows:
+            if window != original_window:
+                self.driver.switch_to.window(window)
+                break
+        
         return original_window
+
+    def switch_to_window(self, window_handle):
+        self.driver.switch_to.window(window_handle)
 
     def get_current_url(self):
         return self.driver.current_url
+
+    def scroll_to_element(self, locator, time=10):
+        element = self.find_element(locator, time)
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        return element
+
+    def is_element_visible(self, locator, time=5):
+        try:
+            return WebDriverWait(self.driver, time).until(EC.visibility_of_element_located(locator)).is_displayed()
+        except:
+            return False
